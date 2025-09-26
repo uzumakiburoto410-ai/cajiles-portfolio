91a6eaf24969b165a9d6e7d07dca662fca41f0ac
Alvin M. Cajiles <uzumakiburoto410@gmail.com>
2025-09-26 14:29:05 +0800

    Delete Cajiles_Alvin_Portfolio_MidtermProject/main.js

--- Cajiles_Alvin_Portfolio_MidtermProject/main.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('nav a').forEach(link =>{
-    link.addEventListener('click', function(e){
-        e.preventDefault();
-        const target  = document.querySelector(this.getAttribute('href'));
-        if(target){
-            target.scrollIntoView({behavior:'smooth'});
-        }
-    });
-});
